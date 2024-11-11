97769ca83117b300d2e0ce93c112c8d326831aa2
Gordon J. Köhn <gordon@koehn.net>
2024-11-11 12:23:05 +0000

    up the code quality

--- src/sr2silo/process.py
+++ src/sr2silo/process.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 def parse_cigar(cigar: str) -> list[tuple[str, int]]:
-    """Parse a CIGAR string into a list of tuples."""
+    """Parse a cigar string into a list of tuples."""
     pattern = re.compile(r"(\d+)([MIDNSHP=X])")
 
     parsed_cigar = pattern.findall(cigar)
@@ -37,72 +37,72 @@ def pair_normalize_reads(sam_data: str, output_fasta: Path, output_insertions: P
 
             fields = line.strip().split("\t")
 
-            QNAME = fields[0]  # Query template NAME
-            POS = int(fields[3])  # 1-based leftmost mapping POSition
-            CIGAR = parse_cigar(fields[5])  # CIGAR string
-            SEQ = fields[9]  # segment SEQuence
-            QUAL = fields[10]  # ASCII of Phred-scaled base QUALity + 33
+            qname = fields[0]  # Query template NAME
+            pos = int(fields[3])  # 1-based leftmost mapping position
+            cigar = parse_cigar(fields[5])  # cigar string
+            seq = fields[9]  # segment sequence
+            qual = fields[10]  # ASCII of Phred-scaled base quality + 33
 
             result_sequence = ""
             result_qual = ""
             index = 0
             inserts = []
 
-            for operation in CIGAR:
-                type, count = operation
-                if type == "S":
+            for operation in cigar:
+                ops_type, count = operation
+                if ops_type == "S":
                     index += count
                     continue
-                if type == "M":
-                    result_sequence += SEQ[index : index + count]
-                    result_qual += QUAL[index : index + count]
+                if ops_type == "M":
+                    result_sequence += seq[index : index + count]
+                    result_qual += qual[index : index + count]
                     index += count
                     continue
-                if type == "D":
+                if ops_type == "D":
                     result_sequence += "-" * count
                     result_qual += "!" * count
                     continue
-                if type == "I":
-                    inserts.append((index + POS, SEQ[index : index + count]))
+                if ops_type == "I":
+                    inserts.append((index + pos, seq[index : index + count]))
                     index += count
                     continue
 
             read = {
-                "POS": POS,
-                "CIGAR": CIGAR,
-                "RESULT_SEQUENCE": result_sequence,
-                "RESULT_QUAL": result_qual,
+                "pos": pos,
+                "cigar": cigar,
+                "RESULT_seqUENCE": result_sequence,
+                "RESULT_qual": result_qual,
                 "insertions": inserts,
             }
 
-            if QNAME in unpaired:
-                read1 = unpaired.pop(QNAME)
+            if qname in unpaired:
+                read1 = unpaired.pop(qname)
                 read2 = read
 
-                if read1["POS"] > read2["POS"]:
+                if read1["pos"] > read2["pos"]:
                     read1, read2 = read2, read1
 
-                index = read1["POS"]
-                read1len = len(read1["RESULT_SEQUENCE"])
-                merged = read1["RESULT_SEQUENCE"][: min(read1len, read2["POS"] - read1["POS"])]
+                index = read1["pos"]
+                read1len = len(read1["RESULT_seqUENCE"])
+                merged = read1["RESULT_seqUENCE"][: min(read1len, read2["pos"] - read1["pos"])]
 
                 # do deletions cause a problem here?
-                gaplen = read1["POS"] + read1len - read2["POS"]
+                gaplen = read1["pos"] + read1len - read2["pos"]
                 if gaplen < 0:
                     merged += "N" * (-gaplen)
-                    merged += read2["RESULT_SEQUENCE"]
+                    merged += read2["RESULT_seqUENCE"]
                 else:
-                    overlap_read1 = read1["RESULT_SEQUENCE"][read2["POS"] - read1["POS"] :]
-                    overlap_read2 = read2["RESULT_SEQUENCE"][0 : max(0, gaplen)]
+                    overlap_read1 = read1["RESULT_seqUENCE"][read2["pos"] - read1["pos"] :]
+                    overlap_read2 = read2["RESULT_seqUENCE"][0 : max(0, gaplen)]
 
-                    overlap_qual1 = read1["RESULT_QUAL"][read2["POS"] - read1["POS"] :]
-                    overlap_qual2 = read2["RESULT_QUAL"][0 : max(0, gaplen)]
+                    overlap_qual1 = read1["RESULT_qual"][read2["pos"] - read1["pos"] :]
+                    overlap_qual2 = read2["RESULT_qual"][0 : max(0, gaplen)]
 
                     # let's set the read1's version by default
                     overlap_result = list(overlap_read1)
 
-                    if len(overlap_result) and overlap_read1 != overlap_read2:
-                        # print("", QNAME)
+                    if overlap_result and overlap_read1 != overlap_read2:
+                        # print("", qname)
                         if len(overlap_read1) != len(overlap_read2):
                             print("overlaps don't match in size")
                         number_of_diffs = 0
@@ -115,21 +115,21 @@ def pair_normalize_reads(sam_data: str, output_fasta: Path, output_insertions: P
                                 # print("diff in position ", i, ": ", overlap_read1[i], "/", overlap_read2[i])
                                 number_of_diffs += 1
 
-                    merged += "".join(overlap_result) + read2["RESULT_SEQUENCE"][max(0, gaplen) :]
+                    merged += "".join(overlap_result) + read2["RESULT_seqUENCE"][max(0, gaplen) :]
 
-                if len(merged) != read2["POS"] + len(read2["RESULT_SEQUENCE"]) - read1["POS"]:
+                if len(merged) != read2["pos"] + len(read2["RESULT_seqUENCE"]) - read1["pos"]:
                     raise Exception("Length mismatch")
 
-                fasta_file.write(f">{QNAME}|{read1['POS']}\n{merged}\n")
+                fasta_file.write(f">{qname}|{read1['pos']}\n{merged}\n")
 
                 merged_insertions = read1["insertions"].copy()
-                insertion_index = read1["POS"] + read1len
+                insertion_index = read1["pos"] + read1len
                 merged_insertions += [insert for insert in read2["insertions"] if insert[0] > insertion_index]
 
-                insertions_file.write(f"{QNAME}\t{merged_insertions}\n")
+                insertions_file.write(f"{qname}\t{merged_insertions}\n")
 
             else:
-                unpaired[QNAME] = read
+                unpaired[qname] = read
         for id in unpaired:
-            fasta_file.write(f">{id}|{unpaired[id]['POS']}\n{unpaired[id]['RESULT_SEQUENCE']}\n")
+            fasta_file.write(f">{id}|{unpaired[id]['pos']}\n{unpaired[id]['RESULT_seqUENCE']}\n")
             insertions_file.write(f"{id}\t{unpaired[id]['insertions']}\n")
