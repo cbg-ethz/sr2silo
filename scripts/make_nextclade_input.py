60b91c5da6c21264b9f9c104c0e96845ef4d9685
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-23 09:48:13 +0100

    make nextclade input

---
+++ scripts/make_nextclade_input.py
@@ -0,0 +1,34 @@
+"""Make cleartext alignment from BAM file with reference sequence
+    for Nextclade input."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from sr2silo.process.convert import bam_to_cleartext_alignment
+
+REFERENCE_PATH = Path("resources/sars-cov-2/NC_045512.2.fasta")
+OUTPUT_PATH = Path("out/nextclade/nextclade_input.ndjson")
+BAM_PATH = Path(
+    "tests/data/samples_large/A1_05_2024_10_08/20241024_2411515907/alignments/REF_aln_trim.bam"
+)
+
+
+def process(bam_path: Path, output_fp: Path, reference: Path) -> None:
+    """Convert BAM to cleartext alignment and write to a ndjson file.
+
+    One Json per read with elements
+    - read_id: read identifier
+    - query_seq_aligned: the aligned sequence with gaps
+    - inserts: list of insertions with elements
+            - pos: position of the insertion
+            - ins: insertion sequence
+    """
+    # check if the output directory exists
+    output_fp.parent.mkdir(parents=True, exist_ok=True)
+
+    bam_to_cleartext_alignment(bam_path, output_fp, reference)
+
+
+if __name__ == "__main__":
+    process(BAM_PATH, OUTPUT_PATH, REFERENCE_PATH)
