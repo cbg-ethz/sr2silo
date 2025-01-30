5dd2029533ff65211f6d59ce919044eb387d2a62
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-30 16:14:45 +0100

    add pseudo code to script

--- blastX/aa_sam_to_geneJson.py
+++ blastX/aa_sam_to_geneJson.py
@@ -3,18 +3,21 @@ from __future__ import annotations
 
 ## read in each line in the sam file
 
-## if the read_id is not in the dictionary, add it
+### if the read_id is not in the dictionary, add it
 
-## make a dictionary for AA_sequences for the gene
+###### make a dictionary for AA_sequences for the gene
 
-## make a dictionary for insertions for the gene
+###### make a dictionary for insertions for the gene
 
-## get the gene name from the reference name
+### get the gene name from the reference name
 
-## get the cleartext sequence from the aligned query sequence and the cigar string
+### get the cleartext sequence from the aligned query sequence and the cigar string
 
-## get the insertions from the aligned query sequence and the cigar string
+### get the insertions from the aligned query sequence and the cigar string
 
-## add the gene name: sequence, and insertions to the dictionaries
+### add the gene name: sequence, and insertions to the dictionaries
 
 ## write the dictionaries to a json file
+
+
+
