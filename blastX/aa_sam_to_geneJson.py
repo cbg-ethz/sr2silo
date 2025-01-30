64f9880acab1354af938902a3cccc214f2d95153
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-30 17:12:10 +0100

    get gene names

--- blastX/aa_sam_to_geneJson.py
+++ blastX/aa_sam_to_geneJson.py
@@ -1,24 +1,6 @@
 # Script to convert SAM to json gene, sequence insertions
 from __future__ import annotations
 
-## read in each line in the sam file
-
-### if the read_id is not in the dictionary, add it
-
-###### make a dictionary for AA_sequences for the gene
-
-###### make a dictionary for insertions for the gene
-
-### get the gene name from the reference name
-
-### get the cleartext sequence from the aligned query sequence and the cigar string
-
-### get the insertions from the aligned query sequence and the cigar string
-
-### add the gene name: sequence, and insertions to the dictionaries
-
-## write the dictionaries to a json file
-
 
 def parse_cigar(cigar):
     """Parse the CIGAR string into a list of tuples."""
@@ -107,11 +89,142 @@ def process_sequence(seq, cigar):
     return "".join(cleartext_sequence), insertions, deletions
 
 
-# Example usage:
-sequence = "AGCTTCAGCTAGCTT"
-cigar = "5M2I5M1D3M"
-cleartext, insertions, deletions = process_sequence(sequence, cigar)
+# TODO: validate the long names
+protein_names = {
+    "YP_009742608.1": "leader protein",
+    "YP_009742609.1": "nsp2",
+    "YP_009742610.1": "nsp3",
+    "YP_009742611.1": "nsp4",
+    "YP_009742612.1": "3C-like proteinase",
+    "YP_009742613.1": "nsp6",
+    "YP_009742614.1": "nsp7",
+    "YP_009742615.1": "nsp8",
+    "YP_009742616.1": "nsp9",
+    "YP_009742617.1": "nsp10",
+    "YP_009725318.1": "ORF7b",
+    "YP_009725295.1": "ORF1a polyprotein",
+    "YP_009725297.1": "leader protein",
+    "YP_009725298.1": "nsp2",
+    "YP_009725299.1": "nsp3",
+    "YP_009725300.1": "nsp4",
+    "YP_009725301.1": "3C-like proteinase",
+    "YP_009725302.1": "nsp6",
+    "YP_009725303.1": "nsp7",
+    "YP_009725304.1": "nsp8",
+    "YP_009725305.1": "nsp9",
+    "YP_009725306.1": "nsp10",
+    "YP_009725307.1": "RNA-dependent RNA polymerase",
+    "YP_009725308.1": "helicase",
+    "YP_009725309.1": "3'-to-5' exonuclease",
+    "YP_009725310.1": "endoRNAse",
+    "YP_009725311.1": "2'-O-ribose methyltransferase",
+    "YP_009725312.1": "nsp11",
+    "YP_009725255.1": "ORF10 protein",
+    "YP_009724389.1": "ORF1ab polyprotein",
+    "YP_009724390.1": "surface glycoprotein",
+    "YP_009724391.1": "ORF3a protein",
+    "YP_009724392.1": "envelope protein",
+    "YP_009724393.1": "membrane glycoprotein",
+    "YP_009724394.1": "ORF6 protein",
+    "YP_009724395.1": "ORF7a protein",
+    "YP_009724396.1": "ORF8 protein",
+    "YP_009724397.2": "nucleocapsid phosphoprotein",
+}
 
-print("Cleartext Sequence:", cleartext)
-print("Insertions:", insertions)
-print("Deletions:", deletions)
+# TODO: validate the short names
+short_names = {
+    "leader protein": "leader",
+    "nsp2": "nsp2",
+    "nsp3": "nsp3",
+    "nsp4": "nsp4",
+    "3C-like proteinase": "3CLpro",
+    "nsp6": "nsp6",
+    "nsp7": "nsp7",
+    "nsp8": "nsp8",
+    "nsp9": "nsp9",
+    "nsp10": "nsp10",
+    "ORF7b": "ORF7b",
+    "ORF1a polyprotein": "ORF1a",
+    "RNA-dependent RNA polymerase": "RdRp",
+    "helicase": "Helicase",
+    "3'-to-5' exonuclease": "ExoN",
+    "endoRNAse": "EndoU",
+    "2'-O-ribose methyltransferase": "NMT",
+    "nsp11": "nsp11",
+    "ORF10 protein": "ORF10",
+    "ORF1ab polyprotein": "ORF1ab",
+    "surface glycoprotein": "S",
+    "ORF3a protein": "ORF3a",
+    "envelope protein": "E",
+    "membrane glycoprotein": "M",
+    "ORF6 protein": "ORF6",
+    "ORF7a protein": "ORF7a",
+    "ORF8 protein": "ORF8",
+    "nucleocapsid phosphoprotein": "N",
+}
+
+protein_short_names = {}
+
+for key, value in protein_names.items():
+    if value in short_names:
+        protein_short_names[key] = short_names[value]
+    else:
+        protein_short_names[
+            key
+        ] = value  # Use the full name if no short name is available
+
+print(protein_short_names)
+
+#### PSUEDOCODE ####
+
+## read in each line in the sam file
+
+### if the read_id is not in the dictionary, add it
+
+###### make a dictionary for AA_sequences for the gene
+
+###### make a dictionary for insertions for the gene
+
+### get the gene name from the reference name
+
+### get the cleartext sequence from the aligned query sequence and the cigar string
+
+### get the insertions from the aligned query sequence and the cigar string
+
+### add the gene name: sequence, and insertions to the dictionaries
+
+## write the dictionaries to a json file
+
+
+INPUT_FILE = "diamond_blastx.sam"
+
+# make a dictionary for the AA_sequences
+aa_sequences = {}
+
+# make a dictionary for the insertions
+aa_insertions = {}
+
+with open(INPUT_FILE, "r") as f:
+    for line in f:
+        # Skip header lines
+        if line.startswith("@"):
+            continue
+        # Split the line into fields
+        fields = line.strip().split("\t")
+        read_id = fields[0]
+        gene_name = fields[2]
+        cigar = fields[5]
+        seq = fields[9]
+
+        cleartext, insertions, deletions = process_sequence(seq, cigar)
+
+        if (
+            gene_name not in aa_sequences.keys()
+            and gene_name not in aa_insertions.keys()
+        ):
+            aa_sequences[gene_name] = ""
+            aa_insertions[gene_name] = ""
+
+
+for ref_name in aa_sequences.keys():
+    print(protein_short_names[ref_name])
