b9693e8ae8de13f80c353e6021c83c24b8d9b464
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-02-10 14:30:25 +0100

    moving conversion to process module

--- src/sr2silo/process/convert.py
+++ src/sr2silo/process/convert.py
@@ -10,6 +10,72 @@ from typing import List, Union
 
 import pysam
 
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def sort_bam_file(input_bam_path: Path, output_bam_path: Path):
+    """
+    Sorts a BAM file using pysam.sort to avoid loading all alignments into memory.
+
+    Args:
+        input_bam_path (Path): Path to the input BAM file.
+        output_bam_path (Path): Path to the output sorted BAM file.
+    """
+    try:
+        # Convert Path objects to strings for pysam compatibility
+        input_bam_str = str(input_bam_path)
+        output_bam_str = str(output_bam_path)
+
+        # Using pysam.sort command to sort the BAM file and write to disk incrementally.
+        pysam.sort("-o", output_bam_str, input_bam_str)
+        print(f"BAM file has been sorted and saved to {output_bam_str}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise Exception(f"An error occurred: {e}")
+
+
+def create_index(bam_file: Path):
+    """
+    Create an index for a BAM file using pysam.
+
+    Args:
+        bam_file (str): Path to the input BAM file.
+    """
+    try:
+        # Convert Path object to string for pysam compatibility
+        bam_file_str = str(bam_file)
+
+        # Open BamFile and save with 'bai' extension
+        pysam.index(bam_file_str)
+        print(f"Index created for {bam_file}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def bam_to_fasta(bam_file, fasta_file):
+    """
+    Convert a BAM file to a FASTA file. Bluntly resolved the sam to fasta.
+
+    Args:
+        bam_file (str): Path to the input BAM file.
+        fasta_file (str): Path to the output FASTQ file.
+    """
+    # check for proper format
+    if not bam_file.endswith(".bam"):
+        raise ValueError("Input file is not a BAM file")
+    if not fasta_file.endswith(".fasta"):
+        raise ValueError("Output file is not a FASTA file")
+
+    with pysam.AlignmentFile(bam_file, "rb") as bam:
+        with open(fasta_file, "w") as fq:
+            for read in bam.fetch():
+                if not read.is_unmapped:
+                    name = read.query_name
+                    seq = read.query_sequence
+                    fq.write(f">{name}\n{seq}\n")
+
 
 def bam_to_sam(bam_file: Path) -> str:
     """Converts a BAM file to SAM format and returns it as a string.
@@ -32,6 +98,88 @@ def bam_to_sam(bam_file: Path) -> str:
     return temp_sam_content
 
 
+def bam_to_fastq_handle_indels(
+    bam_file, fastq_file, insertions_file, deletion_char="-"
+):
+    """
+    Convert a BAM file to a FASTQ file, removing insertions and adding a special character for deletions.
+    Save the insertions to a separate file. Include alignment positions in the FASTQ file.
+
+    Used to look at the cleartext nucleotide sequence of the reads.
+
+    :param bam_file: Path to the input BAM file
+    :param fastq_file: Path to the output FASTQ file
+    :param insertions_file: Path to the output file containing insertions
+    :param deletion_char: Special character to use for deletions
+    """
+    with pysam.AlignmentFile(bam_file, "rb") as bam, open(
+        fastq_file, "w"
+    ) as fastq, open(insertions_file, "w") as insertions:
+        for read in bam.fetch():
+            if not read.is_unmapped:
+                query_sequence = read.query_sequence
+                query_qualities = read.query_qualities
+                new_sequence = []
+                new_qualities = []
+                insertion_positions = []
+
+                query_pos = 0
+                ref_align_start = read.reference_start
+                ref_pos = read.reference_start
+
+                for cigar in read.cigartuples:
+                    if cigar[0] == 0:  # Match or mismatch
+                        new_sequence.extend(
+                            query_sequence[query_pos : query_pos + cigar[1]]
+                        )
+                        new_qualities.extend(
+                            query_qualities[query_pos : query_pos + cigar[1]]
+                        )
+                        query_pos += cigar[1]
+                        ref_pos += cigar[1]
+                    elif cigar[0] == 1:  # Insertion
+                        insertion_seq = query_sequence[query_pos : query_pos + cigar[1]]
+                        insertion_qual = [
+                            chr(q + 33)
+                            for q in query_qualities[query_pos : query_pos + cigar[1]]
+                        ]
+                        insertion_positions.append(
+                            (ref_pos, insertion_seq, insertion_qual)
+                        )
+                        query_pos += cigar[1]
+                    elif cigar[0] == 2:  # Deletion
+                        new_sequence.extend([deletion_char] * cigar[1])
+                        new_qualities.extend(
+                            [0] * cigar[1]
+                        )  # Assigning a low-quality score for deletions
+                        ref_pos += cigar[1]
+
+                # Write the modified read to the FASTQ file
+                fastq.write(f"@{read.query_name}\n")
+                fastq.write(f"{''.join(new_sequence)}\n")
+                fastq.write("+\n")
+                fastq.write(
+                    f"{''.join(chr(q + 33) for q in new_qualities)}\n"
+                )  # Phred33 encoding
+
+                # Write the alignment positions to the FASTQ file
+                fastq.write(f"alignment_position:{ref_align_start}\n")
+
+                # Write the insertions to the insertions file
+                for insertion_pos, insertion_seq, insertion_qual in insertion_positions:
+                    insertions.write(
+                        f"{read.query_name}\t{insertion_pos}\t{''.join(insertion_seq)}\t{''.join(insertion_qual)}\n"
+                    )
+
+
+def parse_cigar_new(cigar: str) -> List[Tuple[int, str]]:
+    """Parse the CIGAR string into a list of tuples."""
+    return [
+        (int(length), op) for length, op in re.findall(r"(\d+)([MIDNSHP=X])", cigar)
+    ]
+
+
+# TODO identify where needed and remove this function
 def parse_cigar(cigar: str) -> list[tuple[str, int]]:
     """
     Parse a cigar string into a list of tuples.
@@ -51,6 +199,7 @@ def parse_cigar(cigar: str) -> list[tuple[str, int]]:
     return [(op, int(length)) for length, op in parsed_cigar]
 
 
+# TODO: identify where needed, likly duplicate with sam_to_seq_and_indels
 def normalize_reads(sam_data: str, output_fasta: Path, output_insertions: Path) -> None:
     """
     Normalize (to clear text sequence using CIGAR)
