79e3d0853618c22cc639866dc21d6fdb5c50b8a8
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-02-13 12:44:17 +0100

    reformat

--- blastX/aa_sam_to_geneJson.py
+++ blastX/aa_sam_to_geneJson.py
@@ -1,16 +1,19 @@
-"""Script to convert SAM to json gene, sequence insertions
+"""Script to convert BAM to AlignedReads Objects containing all gene information.
+
+   This script will extract nuclitide sequences and insertions from a BAM file and
+    then align them to a reference genome. It will then extract amino acid sequences
+    and amino acid insertions using Diamond / BlastX. Finally, it will create a JSON
+    representation of the AlignedReads objects.
+
+    That looks alike a sinle line the inputs for SILO, excluding the metadata.
 """
 from __future__ import annotations
 
 import logging
 from pathlib import Path
 
-from sr2silo.process.interface import (
-    AlignedRead,
-    GeneName,
-)
-import sr2silo.process.convert as convert
 import sr2silo.process as process
+import sr2silo.process.convert as convert
 import sr2silo.process.translate_align as translate_align
 
 logging.basicConfig(
@@ -20,14 +23,14 @@ logging.basicConfig(
 
 def main():
     """Main function to process SAM files and generate JSON output."""
-    #INPUT_NUC_ALIGMENT_FILE = "input/combined.bam"
-    INPUT_NUC_ALIGMENT_FILE = "input/combined_sorted.bam"
-    # INPUT_NUC_ALIGMENT_FILE = "input/REF_aln_trim.bam"
-    FASTQ_NUC_ALIGMENT_FILE = "output_with_indels.fastq"
-    FASTA_NUC_INSERTIONS_FILE = "output_ins.fasta"
-    AA_ALIGNMENT_FILE = "diamond_blastx.sam"
-    AA_REFERENCE_FILE = "../resources/sars-cov-2/aa_reference_genomes.fasta"
-    NUC_REFERENCE_FILE = "../resources/sars-cov-2/nuc_reference_genomes.fasta"
+    # INPUT_NUC_ALIGMENT_FILE = Path("input/combined.bam")
+    INPUT_NUC_ALIGMENT_FILE = Path("input/combined_sorted.bam")
+    # INPUT_NUC_ALIGMENT_FILE = Path("input/REF_aln_trim.bam")
+    FASTQ_NUC_ALIGMENT_FILE = Path("output_with_indels.fastq")
+    FASTA_NUC_INSERTIONS_FILE = Path("output_ins.fasta")
+    AA_ALIGNMENT_FILE = Path("diamond_blastx.sam")
+    AA_REFERENCE_FILE = Path("../resources/sars-cov-2/aa_reference_genomes.fasta")
+    NUC_REFERENCE_FILE = Path("../resources/sars-cov-2/nuc_reference_genomes.fasta")
 
     # Validate that all Resources are there before starting
     (
@@ -35,13 +38,15 @@ def main():
         AA_REFERENCE_FILE,
         INPUT_NUC_ALIGMENT_FILE,
         AA_ALIGNMENT_FILE,
+        FASTQ_NUC_ALIGMENT_FILE,
     ) = [
         Path(f)
         for f in [
             NUC_REFERENCE_FILE,
             AA_REFERENCE_FILE,
             INPUT_NUC_ALIGMENT_FILE,
-            AA_ALIGNMENT_FILE
+            AA_ALIGNMENT_FILE,
+            FASTQ_NUC_ALIGMENT_FILE,
         ]
     ]
     if not all(
@@ -52,7 +57,9 @@ def main():
 
     # sort and index the input BAM file
     INPUT_NUC_ALIGMENT_FILE_sorted_indexed = Path("input/combined_sorted.bam")
-    convert.sort_and_index_bam(INPUT_NUC_ALIGMENT_FILE, INPUT_NUC_ALIGMENT_FILE_sorted_indexed)
+    convert.sort_and_index_bam(
+        INPUT_NUC_ALIGMENT_FILE, INPUT_NUC_ALIGMENT_FILE_sorted_indexed
+    )
 
     logging.info("Parsing Nucliotide: BAM FASTQ conversion (with INDELS)")
     convert.bam_to_fastq_handle_indels(
@@ -77,14 +84,20 @@ def main():
     logging.info(f"Loaded gene reference with genes: {gene_set}")
 
     logging.info("Processing nucleotide alignments")
-    aligned_reads = translate_align.read_in_AligendReads_nuc_seq(FASTQ_NUC_ALIGMENT_FILE, nuc_reference_length, gene_set)
+    aligned_reads = translate_align.read_in_AligendReads_nuc_seq(
+        FASTQ_NUC_ALIGMENT_FILE, nuc_reference_length, gene_set
+    )
 
     logging.info("Adding nucleotide insertions to reads")
-    aligned_reads = translate_align.read_in_AligendReads_nuc_ins(aligned_reads, FASTA_NUC_INSERTIONS_FILE)
+    aligned_reads = translate_align.read_in_AligendReads_nuc_ins(
+        aligned_reads, FASTA_NUC_INSERTIONS_FILE
+    )
 
     # Process AA alignment file and update corresponding reads
     logging.info("Processing AA alignments")
-    aligned_reads = translate_align.read_in_AlignedReads_aa_seq_and_ins(aligned_reads, AA_ALIGNMENT_FILE, gene_set)
+    aligned_reads = translate_align.read_in_AlignedReads_aa_seq_and_ins(
+        aligned_reads, AA_ALIGNMENT_FILE, gene_set
+    )
 
     for read_id, read in list(aligned_reads.items()):
         print(read)
