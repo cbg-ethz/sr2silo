3beef11a443b1774772017b5c16bdd8168294e4a
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-02-14 14:10:07 +0100

    adding test for nuc insertions

--- tests/process/test_translation_aligment.py
+++ tests/process/test_translation_aligment.py
@@ -2,8 +2,12 @@
 
 from __future__ import annotations
 
+import copy
 import logging
 from pathlib import Path
+from typing import Dict
+
+import pytest
 
 import sr2silo.process.translate_align as translate_align
 from sr2silo.process.translate_align import AlignedRead
@@ -24,8 +28,10 @@ def test_translate():
     assert True
 
 
-def test_parse_translate_align():
-    """Test the parse_translate_align function.
+@pytest.fixture
+def aligned_reads() -> Dict[str, AlignedRead]:
+    """
+    Small mock data with 42 real reads from the combined.bam file.
 
     Current dataset misses Amino Acid Insertions - i.e. not tested here.
     """
@@ -37,6 +43,14 @@ def test_parse_translate_align():
     aligned_reads = translate_align.parse_translate_align(
         nuc_ref_fp, aa_ref_fp, nuc_alignment_fp
     )
+    return aligned_reads
+
+
+def test_parse_translate_align(aligned_reads):
+    """Test the parse_translate_align function.
+
+    Current dataset misses Amino Acid Insertions - i.e. not tested here.
+    """
 
     # load the expected aligned reads
     expected_aligned_reads = []
@@ -68,10 +82,39 @@ def test_read_in_AligendReads_nuc_seq():
     raise NotImplementedError
 
 
-def test_read_in_AligendReads_nuc_ins():
+def test_read_in_AligendReads_nuc_ins(aligned_reads):
     """Test the read_in_AlignedReads_nuc_ins function."""
 
-    raise NotImplementedError
+    fasta_nuc_insertions_file = Path("tests/data/process/nuc_insertions.fasta")
+
+    expected_aligned_reads = translate_align.read_in_AlignedReads_nuc_ins(
+        aligned_reads, fasta_nuc_insertions_file
+    )
+
+    # make list of aligned reads to compare
+    expected_aligned_reads = list(expected_aligned_reads.values())
+
+    actual_aligned_reads = copy.deepcopy(aligned_reads)
+    # set the nucitides to empty lists for comparison
+    for read in actual_aligned_reads.values():
+        read.nucleotide_insertions = []
+
+    # Convert actual_aligned_reads from dict to list for comparison.
+    actual_aligned_reads = list(aligned_reads.values())
+
+    # Compare each read's read_id and nucleotide insertions.
+    for i, (actual, expected) in enumerate(
+        zip(actual_aligned_reads, expected_aligned_reads)
+    ):
+        assert actual.read_id == expected.read_id, (
+            f"Mismatch at read {i} for read_id: "
+            f"Expected {expected.read_id} but got {actual.read_id}"
+        )
+        assert actual.nucleotide_insertions == expected.nucleotide_insertions, (
+            f"Mismatch at read {i} for nucleotide_insertions: "
+            f"Expected {expected.nucleotide_insertions} "
+            f"but got {actual.nucleotide_insertions}"
+        )
 
 
 def test_read_in_AligendReads_aa_ins():
