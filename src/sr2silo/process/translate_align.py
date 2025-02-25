53e32427cf6574b3e42af956b43b2bc2a52890e2
Gordon J Koehn <koehng@ethz.ch>
2025-02-25 19:40:26 +0100

    fix suffix handleing

--- src/sr2silo/process/translate_align.py
+++ src/sr2silo/process/translate_align.py
@@ -471,7 +471,7 @@ def parse_translate_align_in_batches(
         All logs of INFO and below are suppressed.
 
     """
-    if output_fp.suffix != ".ndjson.zst":
+    if output_fp.suffixes != ['.ndjson', '.zst']:
         logging.warning(f"Output file extension changed from {output_fp.suffix} to .ndjson.zst")
         output_fp = output_fp.with_suffix(".ndjson.zst")
 
