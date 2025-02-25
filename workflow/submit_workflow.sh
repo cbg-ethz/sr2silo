8f3e1beca79072ed6ceaaa7d3b3348ba2dd1d654
Gordon J Koehn <koehng@ethz.ch>
2025-02-25 14:47:39 +0100

    euler setup

---
+++ workflow/submit_workflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sbatch \
+    --mail-type=END \
+    --ntasks=1 \
+    --cpus-per-task=24 \
+    --mem-per-cpu=8000 \
+    --time=2:00:00 \
+    -o /cluster/home/koehng/logs/sr2silo.out \
+    -e /cluster/home/koehng/logs/sr2silo.err \
+    snakemake -c 24
