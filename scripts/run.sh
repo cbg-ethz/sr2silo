e827646633a679cc3054876e84ced6cc9bf9b7d2
Gordon Julian Koehn <koehng@ethz.ch>
2025-03-04 16:08:25 +0100

    adding __slots__ (#137)
    Co-authored-by: Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 # Define the arguments
 INPUT_FILE="./tests/data/samples/A1_05_2024_10_08/20241024_2411515907/alignments/REF_aln_trim.bam"
+#INPUT_FILE="../../../data/sr2silo/demo_real/A1_10_2024_09_30/20241018_AAG55WNM5/alignments/REF_aln_trim.bam"
 SAMPLE_ID="A1_05_2024_10_08"
 BATCH_ID="20241024_2411515907"
 OUTPUT_FILE="./results_neo/silo_input.ndjson"
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,9 +3,9 @@
 sbatch \
     --mail-type=END \
     --ntasks=1 \
-    --cpus-per-task=8 \
-    --mem-per-cpu=8000 \
-    --time=6:00:00 \
+    --cpus-per-task=1 \
+    --mem-per-cpu=1600 \
+    --time=2:00:00 \
     -o /cluster/home/koehng/logs/sr2silo.out \
     -e /cluster/home/koehng/logs/sr2silo.err \
-    ./run_vp_transformer.sh
+    ./run.sh
