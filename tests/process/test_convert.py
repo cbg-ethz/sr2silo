2adc9e63de0ba4b81ed02895f57a05449b256ee9
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-03-05 14:06:44 +0100

    removing normalize_reads - unsused Legacy by David

--- tests/process/test_convert.py
+++ tests/process/test_convert.py
@@ -4,8 +4,6 @@ This module contains tests for the conversion functions in the sr2silo package.
 
 from __future__ import annotations
 
-import tempfile
-from pathlib import Path
 from typing import Dict
 
 import pytest
@@ -13,7 +11,6 @@ import pytest
 from sr2silo.process import bam_to_sam
 from sr2silo.process.convert import (
     bam_to_fastq_handle_indels,
-    normalize_reads,
     pad_alignment,
     sam_to_seq_and_indels,
 )
@@ -31,49 +28,6 @@ def test_bam_to_sam(bam_data: Dict):
     assert (
         sam_data == bam_data["sam_data"]
     ), "The converted SAM data does not match the expected SAM data"
-
-
-def test_normalize_reads(sam_with_insert_data):
-    """Test the normalize_reads function normalize_reads"""
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir_path = Path(temp_dir)
-        output_fasta = temp_dir_path / "REF_aln_trim_subsample.fasta"
-        output_insertions = temp_dir_path / "REF_aln_trim_subsample_insertions.fasta"
-
-        # call the function
-        normalize_reads(
-            sam_with_insert_data["sam_data"], output_fasta, output_insertions
-        )
-
-        # check that the output files were created
-        assert output_fasta.exists(), "The output fasta file was not created"
-        assert output_insertions.exists(), "The output insertions file was not created"
-
-        # check that the output files are not empty
-        assert output_fasta.stat().st_size > 0, "The output fasta file is empty"
-        assert (
-            output_insertions.stat().st_size > 0
-        ), "The output insertions file is empty"
-
-        print(f"Output fasta file: {output_fasta}")
-        print(f"Output insertions file: {output_insertions}")
-
-        # check that the output files match the expected files
-
-        with output_fasta.open() as f:
-            output_fasta_str = f.read()
-
-        assert (
-            output_fasta_str == sam_with_insert_data["cleartext"]
-        ), "The output fasta data does not match the expected fasta data"
-
-        with output_insertions.open() as f:
-            output_insertions_str = f.read()
-
-        assert (
-            output_insertions_str == sam_with_insert_data["insertions"]
-        ), "The output insertions data does not match the expected insertions data"
 
 
 @pytest.mark.skip(reason="Not implemented")
