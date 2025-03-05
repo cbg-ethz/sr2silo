bb2a42490538f37531b3159942386610fb1e30e8
Alexander Taepper <alexander.taepper@gmail.com>
2025-03-05 11:50:04 +0100

    clean up code paths

--- merge_sorted_chunks/src/main.rs
+++ merge_sorted_chunks/src/main.rs
@@ -1,13 +1,140 @@
 use clap::Command;
+use itertools::Itertools;
 use serde_json::Value;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::env;
 use std::fs::File;
-use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Read, Write};
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
 use std::path::{Path, PathBuf};
+use std::{env, fs};
 use zstd::stream::Decoder;
 use zstd::Encoder;
+
+fn main() -> std::io::Result<()> {
+    let matches = Command::new("Merge Sorted Chunks")
+        .version("1.0")
+        .author("Alexander Taepper")
+        .arg(
+            clap::Arg::new("sort_field")
+                .long("sort-field")
+                .value_name("FIELD")
+                .help("Specifies the field in the json to sort by")
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("tmp_dir")
+                .long("tmp-directory")
+                .value_name("PATH")
+                .help("Specifies a directory for placing intermediate files"),
+        )
+        .arg(
+            clap::Arg::new("parallel_files")
+                .long("parallel-files")
+                .value_name("u32")
+                .help("Specifies the number of files that should be merged per iteration")
+                .default_value("512"),
+        )
+        .get_matches();
+
+    let tmp_dir = if let Some(given_tmp_dir) = matches.get_one::<String>("tmp_dir") {
+        if fs::exists(given_tmp_dir)? {
+            assert_eq!(
+                fs::read_dir(given_tmp_dir)?.count(),
+                0,
+                "The given tmp directory is not empty"
+            );
+        } else {
+            fs::create_dir(&given_tmp_dir)?
+        };
+        PathBuf::from(given_tmp_dir)
+    } else {
+        env::temp_dir()
+    };
+
+    let parallel_files: usize = matches
+        .get_one::<String>("parallel_files")
+        .unwrap()
+        .parse()
+        .unwrap();
+    assert!(
+        parallel_files > 1,
+        "We need to work on at least 2 files in parallel."
+    );
+
+    let sort_field = matches.get_one("sort_field").unwrap();
+
+    let reader = stdin();
+    let reader = reader.lock();
+
+    let mut merge_iteration = 0;
+
+    let input_files_stdin = BufReader::new(reader)
+        .lines()
+        .map(Result::unwrap)
+        .map(PathBuf::from);
+
+    let mut input_files = merge_files_in_batches(
+        input_files_stdin,
+        &tmp_dir,
+        sort_field,
+        parallel_files,
+        merge_iteration,
+    )?;
+
+    merge_iteration += 1;
+
+    if input_files.len() == 0 {
+        panic!("No input files received");
+    }
+
+    while input_files.len() > parallel_files {
+        input_files = merge_files_in_batches(
+            input_files,
+            &tmp_dir,
+            sort_field,
+            parallel_files,
+            merge_iteration,
+        )?;
+        merge_iteration += 1;
+    }
+
+    merge_files(input_files, &mut stdout().lock(), sort_field)?;
+
+    Ok(())
+}
+
+fn merge_files_in_batches<I>(
+    input_files: I,
+    tmp_dir: &PathBuf,
+    sort_field: &String,
+    batch_size: usize,
+    merge_iteration: usize,
+) -> std::io::Result<Vec<PathBuf>>
+where
+    I: IntoIterator<Item = PathBuf>,
+{
+    let mut next_input_files: Vec<PathBuf> = Vec::new();
+    for (batch_id, batch) in input_files
+        .into_iter()
+        .chunks(batch_size)
+        .into_iter()
+        .enumerate()
+    {
+        let file_name = Path::join(
+            tmp_dir,
+            format!("merged_chunks_{}_{}.ndjson.zst", merge_iteration, batch_id),
+        );
+        let file = File::create(file_name.clone())?;
+        let mut encoder = Encoder::new(file, 3)?;
+
+        merge_files(batch, &mut encoder, sort_field)?;
+
+        encoder.finish()?;
+
+        next_input_files.push(file_name);
+    }
+    Ok(next_input_files)
+}
 
 // Wrapper struct to allow sorting JSON values in a min-heap
 #[derive(Eq, PartialEq)]
@@ -30,15 +157,22 @@ impl PartialOrd for HeapEntry {
 }
 
 // Merging function that reads from readers and writes to any object implementing `Write`
-fn merge_sorted_readers<R: Read, W: Write>(
+fn merge_files<I, W: Write>(
+    files: I,
     output: &mut W,
-    sorted_readers: &mut [BufReader<R>],
     sort_field_name: &String,
-) -> std::io::Result<()> {
+) -> std::io::Result<()>
+where
+    I: IntoIterator<Item = PathBuf>,
+{
     let mut heap = BinaryHeap::new();
 
+    let sorted_readers = files
+        .into_iter()
+        .map(|f| BufReader::new(Decoder::new(File::open(f).unwrap()).unwrap()));
+
     // Store an iterator for each reader
-    let mut reader_iters: Vec<_> = sorted_readers.iter_mut().map(|r| r.lines()).collect();
+    let mut reader_iters: Vec<_> = sorted_readers.into_iter().map(|r| r.lines()).collect();
 
     // Initialize heap with the first line from each reader
     for (index, iter) in reader_iters.iter_mut().enumerate() {
@@ -75,110 +209,4 @@ fn merge_sorted_readers<R: Read, W: Write>(
     }
 
     Ok(())
-}
-
-fn main() -> std::io::Result<()> {
-    let matches = Command::new("Merge Sorted Chunks")
-        .version("1.0")
-        .author("Alexander Taepper")
-        .arg(
-            clap::Arg::new("sort_field")
-                .long("sort-field")
-                .value_name("FIELD")
-                .help("Specifies the field in the json to sort by")
-                .required(true),
-        )
-        .arg(
-            clap::Arg::new("tmp_dir")
-                .long("tmp-directory")
-                .value_name("PATH")
-                .help("Specifies a directory for placing intermediate files"),
-        )
-        .get_matches();
-
-    let tmp_dir = matches
-        .get_one::<String>("tmp_dir")
-        .map(PathBuf::from)
-        .unwrap_or(env::temp_dir());
-
-    let sort_field = matches.get_one("sort_field").unwrap();
-
-    let reader = stdin();
-    let reader = reader.lock();
-
-    let mut input_files: Vec<PathBuf> = Vec::new();
-
-    let mut merge_iteration = 0;
-    let mut chunk_id = 0;
-
-    let mut next_input_files = Vec::new();
-
-    for file in BufReader::new(reader).lines() {
-        let file = file?;
-        if input_files.len() == 512 {
-            let mut sorted_readers = open_zst_files(&mut input_files);
-
-            let file_name = Path::join(
-                &tmp_dir,
-                format!("merged_chunk_{}_{}.ndjson.zst", merge_iteration, chunk_id),
-            );
-
-            let file = File::create(file_name.clone())?;
-            let mut encoder = Encoder::new(file, 3)?;
-
-            merge_sorted_readers(&mut encoder, &mut sorted_readers, sort_field)?;
-
-            encoder.finish()?;
-
-            next_input_files.push(file_name);
-            chunk_id += 1;
-            input_files = Vec::new();
-        }
-
-        input_files.push(PathBuf::from(file));
-    }
-    input_files = next_input_files;
-    merge_iteration += 1;
-
-    if input_files.len() == 0 {
-        panic!("No input files received");
-    }
-
-    while input_files.len() > 1 {
-        let mut next_input_files: Vec<PathBuf> = Vec::new();
-        next_input_files.reserve(input_files.len() / 512);
-        for (chunk_id, chunk) in input_files.chunks_mut(512).enumerate() {
-            let mut sorted_readers = open_zst_files(chunk);
-
-            let file_name = Path::join(
-                &tmp_dir,
-                format!("merged_chunk_{}_{}.ndjson.zst", merge_iteration, chunk_id),
-            );
-            let file = File::create(file_name.clone())?;
-            let mut encoder = Encoder::new(file, 3)?;
-
-            merge_sorted_readers(&mut encoder, &mut sorted_readers, sort_field)?;
-
-            encoder.finish()?;
-
-            next_input_files.push(file_name);
-        }
-        merge_iteration += 1;
-        input_files = next_input_files;
-    }
-
-    println!("Merged chunks to files \"{:?}\"", input_files);
-
-    let mut sorted_readers = open_zst_files(&mut input_files);
-
-    merge_sorted_readers(&mut stdout(), &mut sorted_readers, sort_field)?;
-
-    Ok(())
-}
-
-fn open_zst_files(filenames: &mut [PathBuf]) -> Vec<BufReader<Decoder<'static, BufReader<File>>>> {
-    filenames
-        .iter_mut()
-        .map(|f| BufReader::new(Decoder::new(File::open(f).unwrap()).unwrap()))
-        .collect()
 }
