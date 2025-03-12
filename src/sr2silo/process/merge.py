d936befa471136ac8f2383843839f56235644156
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-03-12 11:26:02 +0100

    WIP

--- src/sr2silo/process/merge.py
+++ src/sr2silo/process/merge.py
@@ -6,6 +6,7 @@ import logging
 import subprocess
 from pathlib import Path
 
+
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -14,6 +15,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# TODO: check that the input file is sorted by QNAME and needs @SQ header
 def paired_end_read_merger(
     nuc_align_sam_fp: Path, ref_genome_fasta_fp: Path, output_merged_sam_fp: Path
 ) -> None:
@@ -29,9 +31,11 @@ def paired_end_read_merger(
     """
 
     command = [
-        paired_end_read_merger,
+        "paired_end_read_merger",
         str(nuc_align_sam_fp),
+        "-f",
         str(ref_genome_fasta_fp),
+        "-o",
         str(output_merged_sam_fp),
     ]
 
@@ -46,3 +50,34 @@ def paired_end_read_merger(
     except Exception as e:
         logger.error(f"Error: {e}")
         raise e
+
+
+def is_bam_sorted_qname(bam_file):
+    """Checks if a BAM file is sorted by genomic coordinates using pysam.
+
+    Args:
+        bam_file (str): Path to the BAM file.
+
+    Returns:
+        bool: True if the BAM file is sorted, False otherwise.
+        Returns None if there's an issue opening the file.
+    """
+    try:
+        bam = pysam.AlignmentFile(bam_file, "rb")  # Open in read-binary mode
+        is_sorted = (
+            bam.header.get("HD", {}).get("SO") == "coordinate"  # pyright: ignore
+        )  # pyright: ignore
+        bam.close()  # Important: Close the file!
+        return is_sorted
+    except ValueError as e:
+        print(f"Error opening BAM file {bam_file}: {e}")  # Handle file errors
+        return None  # Indicate an issue
+    except Exception as e:  # Catch other potential errors (like missing index)
+        print(f"An unexpected error occurred: {e}")
+        return None
+
+
+def had_SQ_header():
+    """Check if the input file has a @SQ header."""
+
+    return NotImplementedError
--- tests/process/test_merge.py
+++ tests/process/test_merge.py
@@ -1,3 +1,20 @@
 """This is a sample python file for testing functions from the source code."""
 
 from __future__ import annotations
+
+from pathlib import Path
+
+from sr2silo.process import paired_end_read_merger
+
+
+def test_paired_end_read_merger():
+
+    SAM_FP = Path("tests/data/bam/combined.sam")
+    REF_GENOME_FP = Path("resources/sars-cov-2/nuc_reference_genomes.fasta")
+
+    output_merged_sam_fp = Path("tests/data/bam/merged.sam")
+
+    paired_end_read_merger(SAM_FP, REF_GENOME_FP, output_merged_sam_fp)
+
+    with open(output_merged_sam_fp, "r") as f:
+        lines = f.readlines()
