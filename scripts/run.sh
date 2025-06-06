dea4ae76b5f7cce1cf83e9be9598137daa88a524
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-06-06 09:34:53 +0200

    bla

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define the arguments
-INPUT_FILE="./tests/data/samples/A1_05_2024_10_08/20241024_2411515907/alignments/REF_aln_trim.bam"
+INPUT_FILE="tests/data/samples_large/A1_05_2024_10_08/20241024_2411515907/alignments/REF_aln_trim.bam"
 #INPUT_FILE="../../../data/sr2silo/demo_real/A1_10_2024_09_30/20241018_AAG55WNM5/alignments/REF_aln_trim.bam"
 SAMPLE_ID="A1_05_2024_10_08"
 BATCH_ID="20241024_2411515907"
 OUTPUT_FILE="./results_neo/silo_input.ndjson"
-TIMELINE_FILE="./tests/data/samples/timeline_A1_05_2024_10_08.tsv"
+TIMELINE_FILE="tests/data/samples_large/timeline_A1_05_2024_10_08.tsv"
 PRIMERS_FILE="./resources/sars-cov-2/primers/primers.yaml"
 NUC_REFERENCE="sars-cov-2"
 
