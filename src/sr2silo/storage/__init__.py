c00ff4b1f2d6fe37a858a58dabbecc9d00fed1ff
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-06-30 14:25:39 +0200

    remove all of storage / add forgotten bump

--- src/sr2silo/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-""" "Implements storage specific actions, such as uploading for S3."""
-
-from __future__ import annotations
-
-from sr2silo.storage.s3_storage import upload_file_to_s3
-
-__all__ = ["upload_file_to_s3"]
