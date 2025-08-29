f2da3c6138a26ede065d9d14aa21af7b2a4c71a8
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-08-29 13:47:23 +0200

    Revert "Update src/sr2silo/loculus/loculus.py"
    This reverts commit e6492dac2fd30f20845f987f9b73acf56e0acb40.

--- src/sr2silo/loculus/loculus.py
+++ src/sr2silo/loculus/loculus.py
@@ -420,7 +420,8 @@ class Submission:
 
     @staticmethod
     def parse_metadata(silo_input: Path) -> Dict:
-        returning all metadata fields but readId as a dictionary with keys
+        """Parses the metadata from a silo input .ndjson.zstd or .ndjson
+        returning all metadata fields but readId as a dictoriary with keys
         in camel case.
 
         Assumptions:
